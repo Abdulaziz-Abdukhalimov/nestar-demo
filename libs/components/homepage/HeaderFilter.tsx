6f6320656335407dcd4629c9daae571504feecbf
Abdulaziz Abdukhalimov <abdukhalimov_front_end@Abdulazizs-MacBook-Pro.local>
2025-10-22 10:04:30 +0900

    feat: publish layout components

---
+++ libs/components/homepage/HeaderFilter.tsx
@@ -0,0 +1,35 @@
+import { Box, Stack } from "@mui/material";
+import ExpandMoreicon from "@mui/icons-material/ExpandMore";
+
+const HeaderFilter = () => {
+  return (
+    <>
+      <Stack className="search-box">
+        <Stack className="select-box">
+          <Box className={"box on"}>
+            <span>Location</span>
+            <ExpandMoreicon />
+          </Box>
+          <Box className={"box "}>
+            <span>Property Type</span>
+            <ExpandMoreicon />
+          </Box>
+          <Box className={"box "}>
+            <span> 7 rooms</span>
+            <ExpandMoreicon />
+          </Box>
+        </Stack>
+        <Stack className="search-box-other">
+          <Box className={"advanced-filter"}>
+            <img src="/img/icons/tune.svg" alt="" />
+            <span>Advanced</span>
+          </Box>
+          <Box className={"search-btn"}>
+            <img src="/img/icons/search_white.svg" alt="" />
+          </Box>
+        </Stack>
+      </Stack>
+    </>
+  );
+};
+export default HeaderFilter;
